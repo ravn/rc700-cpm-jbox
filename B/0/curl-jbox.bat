dc36fabc1b454b6d40b6d72f95905da1dff5a4b2
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2022-12-01 00:26:43 +0100

    Initial commit.

---
+++ B/0/curl-jbox.bat
@@ -0,0 +1,11 @@
+rem  autoloader roms
+curl -O http://www.jbox.dk/rc702/roms/roa375.rom http://www.jbox.dk/rc702/ROB358.MAC http://www.jbox.dk/rc702/roms/rob358.rom
+
+rem other roms
+curl -O http://www.jbox.dk/rc702/roms/roa296.rom http://www.jbox.dk/rc702/roms/roa327.rom http://www.jbox.dk/rc702/roms/rob357.rom
+
+rem CP/M sources
+curl -O http://www.jbox.dk/rc702/source/BIOS.MAC http://www.jbox.dk/rc702/source/BIOSTYPE.MAC http://www.jbox.dk/rc702/source/INIPARMS.MAC http://www.jbox.dk/rc702/source/DANISH.MAC http://www.jbox.dk/rc702/source/INIT.MAC http://www.jbox.dk/rc702/source/CPMBOOT.MAC
+curl -O http://www.jbox.dk/rc702/source/SIO.MAC http://www.jbox.dk/rc702/source/DISPLAY.MAC http://www.jbox.dk/rc702/source/FLOPPY.MAC http://www.jbox.dk/rc702/source/HARDDSK.MAC http://www.jbox.dk/rc702/source/DISKTAB.MAC http://www.jbox.dk/rc702/source/INTTAB.MAC http://www.jbox.dk/rc702/source/PIO.MAC
+curl -O http://www.jbox.dk/rc702/source/OS3BDOS.ASM http://www.jbox.dk/rc702/source/OS2CCP.ASM http://www.jbox.dk/rc702/source/AS0COM.ASM http://www.jbox.dk/rc702/source/AS1IO.ASM http://www.jbox.dk/rc702/source/AS2SCAN.ASM http://www.jbox.dk/rc702/source/AS3SYM.ASM http://www.jbox.dk/rc702/source/AS4SEAR.ASM
+curl -O http://www.jbox.dk/rc702/source/AS5OPER.ASM http://www.jbox.dk/rc702/source/AS6MAIN.ASM http://www.jbox.dk/rc702/source/DUMP.ASM http://www.jbox.dk/rc702/source/FILEX.ASM
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # rc700-cpm-jbox
-RC702 CP/M jbox building on Github Actions, with new stuff.
+RC702 CP/M jbox building on GitHub Actions, with new stuff.
+
+See <http://www.jbox.dk/rc702/rcbios.shtm> for starting point.  
+
+My goal is to end up with what I had 30 years ago, namely a 
+CP/M running on real hardware with serial port and keyboard 
+buffering so it is usable as a terminal against a Unix
+system.   No modem though.  Starting with 19200 baud but
+hopefully go up to 115200 if the CPU can keep up.
+
+
+Instead of continuing with my old disassembled and heavily hacked
+BIOS (see <https://github.com/ravn/rc702-bios>) I am going with 
+the impressive work that Michael Ringgård did with his RC702 emulator.
+
+The sources are in <B/0> to target RunCPM.
+
+/ravn 2022-11-30
