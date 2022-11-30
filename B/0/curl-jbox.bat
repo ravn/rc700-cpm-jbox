c733029d05d36ea193f7dffcccd90410f22e7a62
Thorbjørn Ravn Andersen <thorbjoern@gmail.com>
2022-12-01 00:26:43 +0100

    Initial commit.

---
+++ B/0/curl-jbox.bat
@@ -0,0 +1,11 @@
+rem  autoloader roms
+curl -O http://www.jbox.dk/rc702/roms/roa375.rom -O http://www.jbox.dk/rc702/ROB358.MAC -O http://www.jbox.dk/rc702/roms/rob358.rom
+
+rem other roms
+curl -O http://www.jbox.dk/rc702/roms/roa296.rom -O http://www.jbox.dk/rc702/roms/roa327.rom -O http://www.jbox.dk/rc702/roms/rob357.rom
+
+rem CP/M sources
+curl -O http://www.jbox.dk/rc702/source/BIOS.MAC -O http://www.jbox.dk/rc702/source/BIOSTYPE.MAC -O http://www.jbox.dk/rc702/source/INIPARMS.MAC -O http://www.jbox.dk/rc702/source/DANISH.MAC -O http://www.jbox.dk/rc702/source/INIT.MAC -O http://www.jbox.dk/rc702/source/CPMBOOT.MAC
+curl -O http://www.jbox.dk/rc702/source/SIO.MAC -O http://www.jbox.dk/rc702/source/DISPLAY.MAC -O http://www.jbox.dk/rc702/source/FLOPPY.MAC -O http://www.jbox.dk/rc702/source/HARDDSK.MAC -O http://www.jbox.dk/rc702/source/DISKTAB.MAC -O http://www.jbox.dk/rc702/source/INTTAB.MAC -O http://www.jbox.dk/rc702/source/PIO.MAC
+curl -O http://www.jbox.dk/rc702/source/OS3BDOS.ASM -O http://www.jbox.dk/rc702/source/OS2CCP.ASM -O http://www.jbox.dk/rc702/source/AS0COM.ASM -O http://www.jbox.dk/rc702/source/AS1IO.ASM -O http://www.jbox.dk/rc702/source/AS2SCAN.ASM -O http://www.jbox.dk/rc702/source/AS3SYM.ASM -O http://www.jbox.dk/rc702/source/AS4SEAR.ASM
+curl -O http://www.jbox.dk/rc702/source/AS5OPER.ASM -O http://www.jbox.dk/rc702/source/AS6MAIN.ASM -O http://www.jbox.dk/rc702/source/DUMP.ASM -O http://www.jbox.dk/rc702/source/FILEX.ASM
